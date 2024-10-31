0373a8f7388e398d949e6249814fc25b49c8ce28
DhwaniSheth <19dcs135@charusat.edu.in>
2024-10-30 22:15:23 -0400

    --installation of tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,16 @@
-import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { setUser } from './store/user/user.reducer'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App(): JSX.Element {
-  const dispatch = useDispatch()
-  const [count, setCount] = useState<number>(0)
-  const userObj = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    isAdmin: true,
-  }
-  useEffect(() => {
-    dispatch(setUser(userObj))
-  }, [])
   return (
     <>
-      <h1>Heading</h1>
-      <div className="card">
-        <button onClick={() => setCount((prevCount: number) => prevCount + 1)}>
-          count is {count}
-        </button>
-        {/* <p>{userObj.firstName}</p>
-        <p>{userObj.lastName}</p>
-        <p>{userObj.age}</p> */}
-      </div>
-      <p className="read-the-docs">
-        This should work
-      </p>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
